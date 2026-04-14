0e8485e0e9d5b92a81c35c1659160b3945c3749e
Rac00nCanFly <aleksandra.dziurok@gmail.com>
2026-04-14 12:57:01 +0200

    Deleting temporary files

--- testproject/src/main.py
+++
@@ -1,6 +0,0 @@
-def build_greeting(name:str) -> str: 
-   return f'Hello,{name}!' 
-def main(): 
-   print(build_greeting('world')) 
-if __name__ == '__main__':
-   main()
--- testproject/tests/test_basic.py
+++
@@ -1,3 +0,0 @@
-from src.main import build_greeting 
-def test_build_greeting(): 
-   assert build_greeting("Alice") == "Hello,Alice!"
